134a9a82a2eaf3e120527196005d77915fcd5bd2
Aaran Muraleetharan <aaran.muraleetharan.2@gmail.comaaran.muraleetharan.2@gmail.comaaran.muraleetharan.2@gmail.com>
2023-10-22 18:53:02 -0400

    Commit Banner pt1

--- netflix-build/src/Banner.js
+++ netflix-build/src/Banner.js
@@ -2,16 +2,21 @@ import React from 'react';
 import './Banner.css';
 
 // ************************************************
-//Look into optimizing banner styling into css file
+//  Look into optimizing banner styling into css file
 // ************************************************
 
 function Banner() {
+
+    const truncate = (string, n) => {
+        return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+    }
+
   return (
     <header 
      className='banner' 
      style={{
         backgroundSize: "cover",
-        backgroundImage: `url(https://www.travellingbookjunkie.com/wp-content/uploads/2021/01/14019907992_0544000a68_c.jpg)`,
+        backgroundImage: `url(https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png`,
         backgroundPosition: "center center",
     }}
     >
@@ -21,7 +26,22 @@ function Banner() {
                 <button className='banner__button'>Play</button>
                 <button className='banner__button'>My List</button>
             </div>
+            <h1 className='banner__description'>
+                {truncate(`This is a test description. his is a test description. his is a test description. 
+                his is a test description.his is a test description.his is a test description. his is a test 
+                description. his is a test description. This is a test description. his is a test description. his is a test description. 
+                his is a test description.his is a test description.his is a test description. his is a test 
+                description. his is a test description. This is a test description. his is a test description. his is a test description. 
+                his is a test description.his is a test description.his is a test description. his is a test 
+                description. his is a test description. This is a test description. his is a test description. his is a test description. 
+                his is a test description.his is a test description.his is a test description. his is a test 
+                description. his is a test description. This is a test description. his is a test description. his is a test description. 
+                his is a test description.his is a test description.his is a test description. his is a test 
+                description. his is a test description.`, 150)}
+            </h1>
         </div>
+
+        <div className='banner--fadeBottom'/>
     </header>
   )
 }
